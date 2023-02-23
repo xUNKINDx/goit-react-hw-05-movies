2356467d8333c7efed6a1950ff9bdfc2cac78771
Chumak Slavik <chumakslav@gmail.com>
2023-02-23 18:34:53 +0200

    ready lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
-import NavBar from './NavBar/NavBar';
-import MovieDetails from './MovieDetails/MovieDetails';
-import Cast from './MovieDetails/Cast/Cast';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import Reviews from './MovieDetails/Reviews/Reviews';
-import Movies from './Movies/Movies';
+import { lazy, Suspense } from 'react';
+import NavBar from './NavBar/NavBar';
+import Loader from './Loader/Loader';
+
+const Movies = lazy(() => import('./Movies/Movies'));
+const Home = lazy(() => import('./Home/Home'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./MovieDetails/Cast/Cast'));
+const Reviews = lazy(() => import('./MovieDetails/Reviews/Reviews'));
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
